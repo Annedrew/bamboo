943c7458ff60f4b0bcfb24f0451e86b0e7b05797
Annedrew <1196244890@qq.com>
2025-01-20 21:38:22 +0100

    Update lca with foreground notebook and code.

--- bw_exiobase/foreground_importer.py
+++ bw_exiobase/foreground_importer.py
@@ -19,24 +19,27 @@ class ForegroundImporter:
         bifg = np.zeros([len(emissions), len(fg_activities)])
 
         for index, row in extend_data.iterrows():
-            # check which column the exchange is in
             activity_name = row["Activity name"]
-            column_count = fg_activities.index(activity_name)
+            column_num = fg_activities.index(activity_name)
 
             if row["Exchange type"] == "production":  # fgfg
-                np.fill_diagonal(fgfg, 1)
-                # TODO: add amount
+                row_num = fg_activities.index(row["Exchange name"])
+                fgfg[row_num][column_num] = row["Exchange amount"]
             elif row["Exchange type"] == "technosphere": # bgfg
-                row_count = bg_activities.index(row["Exchange name"])
-                fgbg[row_count][column_count] = row["Exchange amount"]
+                row_num = bg_activities.index(row["Exchange name"])
+                bgfg[row_num][column_num] = row["Exchange amount"]
             elif row["Exchange type"] == "biosphere": # bifg
-                row_count = emissions.index(row["Exchnage name"])
-                bifg[row_count][column_count] = row["Exchange amount"]
+                row_num = emissions.index(row["Exchange name"])
+                bifg[row_num][column_num] = row["Exchange amount"]
+
+        fgfg = np.nan_to_num(fgfg, nan=0)
+        bgfg = np.nan_to_num(bgfg, nan=0)
+        bifg = np.nan_to_num(bifg, nan=0)
 
         return fgbg, fgfg, bgfg, bifg
 
     def concatenate_matrix(self, tech_matrix: pd.DataFrame, bio_matrix: pd.DataFrame, fgbg: np.ndarray, fgfg: np.ndarray, bgfg: np.ndarray, bifg: np.ndarray):
-        tech_matrix = np.concatenate((np.concatenate((fgfg, fgbg), axis=0), np.concatenate((bgfg, tech_matrix), axis=0)), axis=1)
+        tech_matrix = np.concatenate((np.concatenate((fgfg, bgfg), axis=0), np.concatenate((fgbg, tech_matrix), axis=0)), axis=1)
         bio_matrix = np.concatenate((bifg, bio_matrix), axis=1)
 
         return tech_matrix, bio_matrix
--- constants.py
+++ constants.py
@@ -4,12 +4,11 @@ import os
 # EXIOBASE AGGREGATED FILE PATH
 EXIOBASE_AGGREGATED_INPUT = os.path.join(os.getcwd(), "exiobase_aggregated")
 EXIOBASE_AGGREGATED_OUTPUT = os.path.join(os.getcwd(), "small_output")
-EXIOBASE_AGGREGATED_A_FILE = os.path.join(os.getcwd(), "notebooks/data/exiobase_aggregated", "A.txt")
-EXIOBASE_AGGREGATED_S_FILE = os.path.join(os.getcwd(), "notebooks/data/exiobase_aggregated", "satellite", "S.txt")
-EMISSION_CODE_FILE = os.path.join(os.getcwd(), "notebooks/data", "EXIOBASE-ecoinvent-bio-bw-GHG.csv")
-
-# SELECTED ACTIVITIES
-SELECTED_AGGREGATED = [("RoW-Services", 68), ("EU28-Biodiesels", 11), ("EU28-Agriculture-Forestry-Fishing", 0), ("EU28-Basic iron and steel and of ferro-alloys and first products thereof", 13), ("EU28-Energy", 1), ("RoW-Waste management", 71), ("EU28-Services", 30), ("EU28-Industry", 3)]
+EXIOBASE_AGGREGATED_A_FILE = os.path.join(os.getcwd(), "data/", "A.txt")
+EXIOBASE_AGGREGATED_S_FILE = os.path.join(os.getcwd(), "data/", "S.txt")
+EMISSION_CODE_FILE = os.path.join(os.getcwd(), "data", "EXIOBASE-ecoinvent-bio-bw-GHG.csv")
+FG_FILE_1COL = os.path.join(os.getcwd(), "data", "fg_exiobase_aggregated_1col.csv")
+FG_FILE_2COL = os.path.join(os.getcwd(), "data", "fg_exiobase_aggregated_2col.csv")
 
 # UNCERTAINTY
 DIST_TYPE = ["static", "uniform", "log-normal", "pedigree"] # Define the types of distribution
@@ -17,8 +16,7 @@ U_UNIFORM = [0.1, 0.2, 0.3] # Define the uncertainty for uniform distribution
 U_LOG = [1.106, 1.225, 1.363] # Define the uncertainty for log-normal distribution
 
 # ---------- CONSTANTS FOR CASE STUDY ----------
-METHOD = ('EF v3.1', 'climate change', 'global warming potential (GWP100)')
-METHOD_IPCC = ('IPCC 2013', 'climate change', 'global warming potential (GWP100)')
+METHOD = ('IPCC 2013', 'climate change', 'global warming potential (GWP100)')
 GHG = ["CO2 - combustion - air",
         "CO2 - non combustion - Cement production - air",
         "CO2 - non combustion - Lime production - air",
@@ -40,17 +38,7 @@ GHG = ["CO2 - combustion - air",
         "N2O - agriculture - air",
         "SF6 - air"]
 
-CFS = [1., 1., 1., 1., 1., 1., 27.0, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 29.8, 273., 273., 25200.]
-
 COUNTRY_FILE = "../gsd_data/Grouping_reg.csv"
 SECTOR_FILE = "../gsd_data/Grouping_sec.csv"
 GSD_FILE = "../gsd_data/GSD_sec_reg.csv"
 GSD_SMALL_FILE = "../gsd_data/GSD_background_pedigree_exiobase_small.csv"
-
-SMALL_EXTEND = "../extend_data/20241121_foreground_system_small.csv"
-
-# ---------- CONSTANTS FOR PLOT DRAWING ----------
-FOLDER_PATH = os.path.join(os.getcwd(), "Exiobase_folders_for_Ning")
-DB_SIZE = ["small", "big"]
-PLT_COMP_TYPE = ["cases", "sectors"]
-PLT_SAVE_PATH = os.path.join(FOLDER_PATH, "plt_output")
